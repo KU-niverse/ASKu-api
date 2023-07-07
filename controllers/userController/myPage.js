0c6b586268a395a63e7d5b757db496a68b769869
youngsupchoi <sup1214@naver.com>
2023-07-07 18:39:56 +0900

    Feat: 유저 정보

---
+++ controllers/userController/myPage.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+const User = require("../../models/userModel");
+const { v4: uuidv4 } = require("uuid");
+const passport = require("passport");
+const express = require("express");
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+var appDir = path.dirname(require.main.filename);
+
+exports.info = async (req, res) => {
+  return res.status(201).json({ success: true, message: req.user[0] });
+};
--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -14,6 +14,7 @@ module.exports = () => {
       },
       async (login_id, password, done) => {
         try {
+          await console.log("실행됨");
           const exUser = await User.findByLoginId(login_id);
           if (exUser.length != 0) {
             const result = await bcrypt.compare(password, exUser[0].password);
