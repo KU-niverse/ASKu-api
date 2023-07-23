3d11aa982b6d2f3f27c976b93cbcfb8cd79a020f
starcat37 <starcat37@korea.ac.kr>
2023-07-23 21:47:49 +0900

    Fix: success 여부 추가

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -23,18 +23,18 @@ exports.reportPostMid = async (req, res, next) => {
 exports.reportCheckPostMid = async (req, res) => {
   try {
     if (!req.body.is_checked) {
-      res.status(406).send({message: "잘못된 확인값입니다."});
+      res.status(406).send({success: false, message: "잘못된 확인값입니다."});
     } else {
       const result = await Report.checkReport(req.body.report_id, req.body.is_checked);
       if (result[0].changedRows) {
-        res.status(200).send({message: "신고를 확인했습니다."}); // 이후 배지 취소 미들웨어 필요
+        res.status(200).send({success: true, message: "신고를 확인했습니다."}); // 이후 배지 취소 미들웨어 필요
         // next();
       } else {
-        res.status(400).send({message: "이미 확인한 신고입니다."});
+        res.status(400).send({success: false, message: "이미 확인한 신고입니다."});
       }
     }
   } catch (err) {
     console.error(err);
-    res.status(404).send({message: "오류가 발생했습니다."});
+    res.status(404).send({success: false, message: "오류가 발생했습니다."});
   }
 };
