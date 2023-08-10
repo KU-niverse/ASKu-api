cdf0a8e4cd43219d0b1313a6ba37613c47f88e6e
youngsupchoi <112808338+youngsupchoi@users.noreply.github.com>
2023-08-10 22:48:56 +0900

    Fix:오타수정

--- models/userModel.js
+++ models/userModel.js
@@ -337,7 +337,7 @@ User.questionHistory = async (user_id, arrange) => {
   //인기순 조회
   else if (arrange == 1) {
     [rows] = await pool.query(
-      `SELECT q.*, w.id AS doc_id, w.title AS doc_title, COUNT(ql.user_id) AS like_count FROM questions q JOIN wiki_docs w ON q.doc_id = w.id LEFT JOIN question_like ql ON q.id = ql.id WHERE q.user_id = 1234 GROUP BY q.id ORDER BY like_count DESC, q.created_at DESC;;`,
+      `SELECT q.*, w.id AS doc_id, w.title AS doc_title, COUNT(ql.user_id) AS like_count FROM questions q JOIN wiki_docs w ON q.doc_id = w.id LEFT JOIN question_like ql ON q.id = ql.id WHERE q.user_id = 1234 GROUP BY q.id ORDER BY like_count DESC, q.created_at DESC;`,
       [user_id]
     );
   }
