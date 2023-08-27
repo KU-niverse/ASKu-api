fcbf729d4c3bd35fec7e8e77cba04616b98f3ef8
youngsupchoi <sup1214@naver.com>
2023-08-27 11:54:40 +0900

    Fix: ai_history테이블에 has_feedback column추가

--- schema.sql
+++ schema.sql
@@ -438,6 +438,7 @@ CREATE TABLE
         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `is_deleted` bool NOT NULL DEFAULT 0,
         -- [히스토리 초기화 여부] 0: 존재 1: 삭제(초기화)된 히스토리
+        `has_feedback` bool NOT NULL DEFAULT 0,
         PRIMARY KEY (`id`),
         FOREIGN KEY (`session_id`) REFERENCES `ai_session` (`id`)
     );
