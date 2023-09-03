e520fdfc57caa45c59fe24b9bb95e31bcd30b66a
youngsupchoi <sup1214@naver.com>
2023-09-03 18:32:54 +0900

    Fix mypage 답변 불러오기 수정

--- models/userModel.js
+++ models/userModel.js
@@ -354,14 +354,32 @@ User.questionHistory = async (user_id, arrange) => {
   let rows;
   if (arrange == 0) {
     [rows] = await pool.query(
-      `SELECT q.*, w.id AS doc_id, w.title AS doc_title, COUNT(ql.user_id) AS like_count FROM questions q JOIN wiki_docs w ON q.doc_id = w.id LEFT JOIN question_like ql ON q.id = ql.id WHERE q.user_id = ? GROUP BY q.id ORDER BY q.created_at DESC;`,
+      `SELECT questions.*, users.nickname, users.rep_badge, badges.image as badge_image, COUNT(question_like.id) AS like_count, wiki_docs.title as doc_title, COUNT(answers.id) AS answer_count
+      FROM questions 
+      INNER JOIN users ON questions.user_id = users.id
+      INNER JOIN wiki_docs ON questions.doc_id = wiki_docs.id
+      INNER JOIN badges ON users.rep_badge = badges.id
+      LEFT JOIN question_like ON questions.id = question_like.id
+      LEFT JOIN answers ON questions.id = answers.question_id
+      WHERE users.id = ?
+      GROUP BY questions.id
+      ORDER BY questions.created_at DESC`,
       [user_id]
     );
   }
   //인기순 조회
   else if (arrange == 1) {
     [rows] = await pool.query(
-      `SELECT q.*, w.id AS doc_id, w.title AS doc_title, COUNT(ql.user_id) AS like_count FROM questions q JOIN wiki_docs w ON q.doc_id = w.id LEFT JOIN question_like ql ON q.id = ql.id WHERE q.user_id = ? GROUP BY q.id ORDER BY like_count DESC, q.created_at DESC;`,
+      `SELECT questions.*, users.nickname, users.rep_badge, badges.image as badge_image, COUNT(question_like.id) AS like_count, wiki_docs.title as doc_title, COUNT(answers.id) AS answer_count
+      FROM questions 
+      INNER JOIN users ON questions.user_id = users.id
+      INNER JOIN wiki_docs ON questions.doc_id = wiki_docs.id
+      INNER JOIN badges ON users.rep_badge = badges.id
+      LEFT JOIN question_like ON questions.id = question_like.id
+      LEFT JOIN answers ON questions.id = answers.question_id
+      WHERE users.id = ?
+      GROUP BY questions.id
+      ORDER BY like_count DESC, questions.created_at DESC`,
       [user_id]
     );
   }
